23eed08a40ade76540e285c044186698fc353894
arvinduh <150882116+arvinduh@users.noreply.github.com>
2025-11-24 21:32:46 -0800

    bugfixes

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,6 +1,11 @@
 """Evaluation module for assessing LLM responses."""
 
-from src.evaluation.deterministic import f1_score
+from src.evaluation.deterministic import (
+  f1_score,
+  meteor_score_eval,
+  rouge_l_score,
+  symbol_precision,
+)
 from src.evaluation.llm_evaluator import LlmEvaluator
 from src.evaluation.models import EvaluationScore
 
@@ -8,4 +13,7 @@ __all__ = [
   "EvaluationScore",
   "f1_score",
   "LlmEvaluator",
+  "meteor_score_eval",
+  "rouge_l_score",
+  "symbol_precision",
 ]
